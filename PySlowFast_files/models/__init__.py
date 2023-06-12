441e9d013a4ef3b6c3deff1e3051557da81ecb04
Francesco Ragusa <64094880+francescoragusa@users.noreply.github.com>
2023-06-12 17:33:12 +0200

    Update __init__.py

--- PySlowFast_files/models/__init__.py
+++ PySlowFast_files/models/__init__.py
@@ -5,4 +5,3 @@ from .build import MODEL_REGISTRY, build_model  # noqa
 from .custom_video_model_builder import *  # noqa
 from .video_model_builder import ResNet, SlowFast  # noqa
 from .custom_video_model_builder_gaze import *
-from .custom_video_model_builder_extract_features import *
